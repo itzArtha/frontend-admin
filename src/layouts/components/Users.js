7f9849d1c89edbfb85d8c1623f77bf673bc3b912
xarthaa <manager.arthachan@gmail.com>
2024-02-09 20:42:11 +0800

    admin user pagination

--- src/layouts/components/Users.js
+++ src/layouts/components/Users.js
@@ -13,7 +13,7 @@ const Users = () => {
 
     const fetchData = (perPage, page, search = '') => {
         apiClient()
-            .get(`/admin/users?page=${page}`)
+            .get(`/admin/users?perPage=${perPage}&page=${page}&search=${search}`)
             .then((r) => {
                 setData(r.data);
             })
@@ -34,8 +34,13 @@ const Users = () => {
     <div>
       <div className={"md:w-1/3 w-full ml-auto my-4"}>
         <MainInput
+            onKeyPress={(e) => {
+                if (e.keyCode === 13) {
+                    fetchData(10, 1, e.target.value)
+                }
+            }}
           onChange={(e) => {
-            setSearch(e.target.value);
+              setSearch(e.target.value);
           }}
           value={search}
           placeholder={"Cari users"}
